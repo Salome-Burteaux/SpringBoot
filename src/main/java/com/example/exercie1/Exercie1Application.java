5b8d87994d4bf55f82751a36eb3de261ddded74a
Salome-Burteaux <salome.burteaux@le-campus-numerique.fr>
2025-02-13 10:14:15 +0100

    first commit

---
+++ src/main/java/com/example/exercie1/Exercie1Application.java
@@ -0,0 +1,13 @@
+package com.example.exercie1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercie1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Exercie1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/exercie1/Exercie1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.exercie1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercie1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
